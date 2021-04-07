c3f786b6ca97d0e43df0d61a895032512f326438
CyberfuzZ <keepthelight@icloud.com>
2021-04-07 16:51:00 +0300

    4.1. Аргументы [#510940]

--- src/ru/job4j/calculator/ArgMethod.java
+++ src/ru/job4j/calculator/ArgMethod.java
@@ -3,7 +3,6 @@ package ru.job4j.calculator;
 public class ArgMethod {
     public static void hello(String name, int age) {
         System.out.println("Hello, " + name);
-        System.out.println(age + " years");
     }
 
     public static void main(String[] args) {
