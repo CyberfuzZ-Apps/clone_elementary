799156bcedbca70f5f2ff800591580858f459335
CyberfuzZ <keepthelight@icloud.com>
2021-04-13 13:29:58 +0300

    5.7. Ипотека [#510989]

--- src/ru/job4j/loop/Mortgage.java
+++ src/ru/job4j/loop/Mortgage.java
@@ -2,8 +2,8 @@ package ru.job4j.loop;
 
 public class Mortgage {
     public static int year(int amount, int salary, double percent) {
-        int year = 1;
-        while (amount + amount / 100 * percent > salary) {
+        int year = 0;
+        while (amount > 0) {
             amount += amount / 100 * percent - salary;
             year++;
         }
